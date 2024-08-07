47a1f12ee72ef938c57f783dab82c180763a9655
ahmed_raza <142502232+AhmedRaza02912@users.noreply.github.com>
2024-08-07 19:42:10 +0500

    Add files via upload

---
+++ book_Ch_4.py
@@ -0,0 +1,62 @@
+# STRINGS ARE IMMNUTABLE, METHODS DONT CHANGE ORGIGNAL STRING
+# name = input("What is your name? ").upper().lstrip()
+# age = int(input("What is your age? "))
+
+# gender = input("What is your gender ").lower()
+
+# if gender == 'f':
+#     gen = ("You are female")
+# elif gender == 'm':
+#     gen = ("You are male")
+# else:
+#     gen = ("Invalid input")
+
+# for i in range(20):
+#     print('=', end='')
+# print()
+# print("PORTFOLIO")
+# for i in range(20):
+#     print('=', end='')
+# print()
+# print("Welcome", name)
+# print("You are",age, "years old")
+# print(gen)
+# name = input("Enter your first name: ")
+# print(name[0].upper())
+# n = 3
+# print(f"{n} is actually large")
+# name = "Ahmed"
+# age = 20
+# gender = "Male"
+# print(f"{name}is {age} years old and is a {gender}")     
+# print("{} is {} years old and is a {}").format(name, age, gender)
+# f1 = "Hamilton down through eau rouge"
+# print(f1.replace("Hamilton","Sebastian"))
+# samantha = "Somebody said something to samantha"
+# print(samantha.replace('s','x'))
+# user = input("Enter your last name: ")
+# if user.find('a') == True:
+#     print("AY YO")
+# else:
+    # print("The required character was not found")
+# WAY 1 to replace several chars
+# inside = input("Enter a string: ")
+# inside = inside.replace('a', '4').replace('b','8').replace('c','3').replace('e','9').replace('d','0').replace('f','14')
+# print(inside)
+# # WAY 2
+# stt = input("Enter a string: ")
+# n = int(input("How many replacements you'd like: "))
+# for i in range(n):
+#     old = input("Enter a char you'd like to replace: ")
+#     new = input("Enter the new char: ")
+#     stt = stt.replace(old, new)
+# print(stt)
+# sibling = input("Enter sibling name: ")
+# substr = input("Enter the sub string: ")
+
+# if sibling.find(substr) != -1:
+#     print(substr, "is present")
+# else:
+#     print(substr,"is not present")
+
+
---
+++ book_Ch_5.py
@@ -0,0 +1,12 @@
+# exponential notation
+# print(1e4)
+# print(1e-4)
+# print(9//3) # int answer
+# print(9/3) # float answer
+# n = 1202.33
+# print(f"value of n with commas is {n:.3f}")
+# ratio = 0.9
+# print(f"ratio is {ratio: .1%}")
+
+return_value = print('What do i return')
+print(return_value)
